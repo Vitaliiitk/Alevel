6bbbd680fa2ea38dbdf1f808819b140441d483ae
Vitalii Tkachenko <ilikecsgo@gmail.com>
2023-11-14 03:41:02 +0200

    Added home task 2.5: new variation of logger with FileService implementation

---
+++ Hometask2.5Logger/Hometask2.5Logger/Program.cs
@@ -0,0 +1,13 @@
+﻿using Hometask2._5Logger.Services;
+using static Hometask2._5Logger.Services.FileService;
+
+namespace Hometask2._5Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            App.Run();   
+        }
+    }
+}

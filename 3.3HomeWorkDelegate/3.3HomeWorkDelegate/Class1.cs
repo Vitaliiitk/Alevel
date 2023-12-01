6746ede259c0af69e6c03bbccc85023ea582b5ea
Vitalii Tkachenko <ilikecsgo@gmail.com>
2023-12-01 05:31:15 +0200

    Added home work 3.3 work with delegates

---
+++ 3.3HomeWorkDelegate/3.3HomeWorkDelegate/Class1.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace _3._3HomeWorkDelegate
+{
+    internal sealed class Class1
+    {
+        public delegate void RefersToShow(bool val);
+
+        public int Multiply(int num1, int num2) => num1 * num2;
+    }
+}
---
+++ 3.3HomeWorkDelegate/3.3HomeWorkDelegate/Class2.cs
@@ -0,0 +1,24 @@
+﻿
+namespace _3._3HomeWorkDelegate
+{
+    public sealed class Class2
+    {
+        public delegate int RefersToMultiply(int num1, int num2);
+        public delegate bool RefersToResult(int num);
+
+        public int multiplyResult { get; set; }
+
+        public RefersToResult Calc(RefersToMultiply multiply, int number1, int number2)
+        {
+            
+            multiplyResult = multiply(number1, number2);
+            RefersToResult resultDelegate = new RefersToResult(Result);
+            return resultDelegate;
+        }
+
+        public bool Result(int arg)
+        {
+            return arg % 2 == 0;
+        }
+    }
+}

3227a2900b8df14f89eb1daba08111427655c92f
Vitalii Tkachenko <ilikecsgo@gmail.com>
2023-12-09 01:13:44 +0200

    Added another home task 3.5CreateAsynchronousMethod

---
+++ 3.5CreateAsynchronousMethod/CreateAsynchronousMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CreateAsynchronousMethod
+{
+    internal sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFromFile concat = new ReadFromFile();
+            string resultedString = concat.ConcatetionOfText().Result;
+            Console.WriteLine(resultedString);
+            
+        }
+    }
+}
---
+++ 3.5CreateAsynchronousMethod/CreateAsynchronousMethod/ReadFromFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAsynchronousMethod
+{
+    public sealed class ReadFromFile
+    {
+        public async Task<string> ReadFromTxt(string txtName) 
+        {
+            var lines = await File.ReadAllLinesAsync(txtName);
+
+
+
+            return lines[0];
+        }
+
+        public async Task<string> ConcatetionOfText()
+        {
+            string lines = await ReadFromTxt("HelloFile.txt") + await ReadFromTxt("WorldFile.txt");
+
+            return lines;
+        }
+    }
+}

e6b74f6aec27aa66d5069f20960fd00af9d195e7
Vitalii Tkachenko <ilikecsgo@gmail.com>
2023-12-10 00:22:29 +0200

    I have added 3.6MessageBox

---
+++ 3.6MessageBox/MessageBox/MessageBox.cs
@@ -0,0 +1,26 @@
+﻿namespace MessageBoxHomeTask
+{
+    public sealed class MessageBox
+    {
+        public event Action<StateEnum> WindowBeenClosed;
+
+        public async void Open()
+        {
+            Console.WriteLine("Window is opened");
+
+            await Task.Delay(3000);
+
+            Console.WriteLine("Window was closed by the user");
+
+            StateEnum state = GetRandomState();
+            WindowBeenClosed?.Invoke(state);
+        }
+
+        private StateEnum GetRandomState()
+        {
+            var random = new Random();
+
+            return (StateEnum)random.Next(0, 2);
+        }
+    }
+}

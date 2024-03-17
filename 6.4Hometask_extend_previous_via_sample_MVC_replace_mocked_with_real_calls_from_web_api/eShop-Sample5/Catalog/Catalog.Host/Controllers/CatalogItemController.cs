c41eb6cf988690efb058853692713570cdf34947
Vitalii Tkachenko <ilikecsgo@gmail.com>
2024-03-17 14:12:31 +0200

    Add hometask 6.4

---
+++ 6.4Hometask_extend_previous_via_sample_MVC_replace_mocked_with_real_calls_from_web_api/eShop-Sample5/Catalog/Catalog.Host/Controllers/CatalogItemController.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using Catalog.Host.Models.Requests;
+using Catalog.Host.Models.Response;
+using Catalog.Host.Services.Interfaces;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Host.Controllers;
+
+[ApiController]
+[Route(ComponentDefaults.DefaultRoute)]
+public class CatalogItemController : ControllerBase
+{
+    private readonly ILogger<CatalogItemController> _logger;
+    private readonly ICatalogItemService _catalogItemService;
+
+    public CatalogItemController(
+        ILogger<CatalogItemController> logger,
+        ICatalogItemService catalogItemService)
+    {
+        _logger = logger;
+        _catalogItemService = catalogItemService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(AddItemResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> AddAsync(CreateProductRequest request)
+    {
+        var result = await _catalogItemService.AddAsync(request.Name, request.Description, request.Price, request.AvailableStock, request.CatalogBrandId, request.CatalogTypeId, request.PictureFileName);
+        return Ok(new AddItemResponse<int?>() { Id = result });
+    }
+
+    [HttpPost]
+    [Route("id")]
+    [ProducesResponseType(typeof(AddItemResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> Update(int id, CreateProductRequest request)
+    {
+        var result = await _catalogItemService.UpdateAsync(id, request.Name, request.Description, request.Price, request.AvailableStock, request.CatalogBrandId, request.CatalogTypeId, request.PictureFileName);
+        return Ok(new AddItemResponse<int?>() { Id = result });
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(typeof(AddItemResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _catalogItemService.DeleteAsync(id);
+
+        return Ok(result);
+    }
+}

44638ca400a075299c431b075897c6156c858b6b
Vitalii Tkachenko <ilikecsgo@gmail.com>
2024-03-23 00:01:00 +0200

    Added hometask 6.6: Identity sever, Basket.Host, 3 methods inside (1 anonymous, 1 with EndUser policy, 1Client policy), MVC calls a method from BasketBffController for details look screenshots folder

---
+++ 6.6.Identity_Server_eShop-Sample6/eShop-Sample6/Catalog/Catalog.Host/Controllers/CatalogBrandController.cs
@@ -0,0 +1,47 @@
+using Catalog.Host.Models.Requests;
+using Catalog.Host.Models.Response;
+using Catalog.Host.Services.Interfaces;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Catalog.Host.Controllers;
+
+[ApiController]
+[Authorize(Policy = AuthPolicy.AllowClientPolicy)]
+[Scope("catalog.catalogBrand")]
+[Route(ComponentDefaults.DefaultRoute)]
+public class CatalogBrandController : ControllerBase
+{
+    private readonly ILogger<CatalogBrandController> _logger;
+    private readonly ICatalogBrandService _catalogBrandService;
+
+    public CatalogBrandController(ILogger<CatalogBrandController> logger, ICatalogBrandService catalogBrandService)
+    {
+        _logger = logger;
+        _catalogBrandService = catalogBrandService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(AddItemResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> Add(CreateBrandRequest request)
+    {
+        var result = await _catalogBrandService.AddAsync(request.Id, request.Brand);
+        return Ok(new AddItemResponse<int?>() { Id = result });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(AddItemResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> UpdateAsync(int id, CreateBrandRequest request)
+    {
+        var result = await _catalogBrandService.UpdateAsync(id, request.Brand);
+        return Ok(new AddItemResponse<int?>() { Id = result });
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(typeof(AddItemResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _catalogBrandService.DeleteAsync(id);
+        return Ok(result);
+    }
+}
---
+++ 6.6.Identity_Server_eShop-Sample6/eShop-Sample6/Web/MVC/ViewModels/Basket.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC.ViewModels
+{
+    public class Basket
+    {
+        public string testString { get; set; }
+    }
+}
